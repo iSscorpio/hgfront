b29c7d8f9e62d345ccbeb54a91e4a0e6b697ff3a
Tane Piper <digitalspaghetti@gmail.com>
2007-12-09 17:47:37 +0000

    Updated admin screens for repos and projects Sign-Off: Tane Piper

--- project/models.py
+++ project/models.py
@@ -8,17 +8,38 @@ import datetime, sys, os, shutil
 
 # Create your models here.
 class Project(models.Model):
+    """A project is a way to group users and repositories"""
     longname=models.CharField(max_length=255)
     shortname=models.CharField(max_length=50)
     description=models.TextField()
     private=models.BooleanField(default=False)
     user=models.ForeignKey(User)
+    pub_date=models.DateTimeField('date published')
+    def __unicode__(self):
+        return self.longname
+    def num_repos(self):
+        """Returns the number of repositories in this project"""
+        return self.repo_set.count()
+    num_repos.short_description = "Number of Repositories"
     class Admin:
-        pass
+        fields = (
+                  ('Project Creation', {'fields': ('longname', 'shortname', 'description',)}),
+                  ('Date information', {'fields': ('pub_date',)}),
+                  ('Publishing Details', {'fields': ('user', 'private',)}),
+        )
+        list_display = ('longname', 'shortname', 'num_repos', 'private', 'user', 'pub_date',)
+        list_filter = ['pub_date', 'private']
+        search_fields = ['longname', 'description']
+        date_hierarchy = 'pub_date'
+        ordering = ('pub_date',)
     
 class Repo(models.Model):
+    """A repo represents a physical repository on the hg system path"""
     name=models.CharField(max_length=20)
     project=models.ForeignKey(Project)
+    pub_date=models.DateTimeField('date published')
+    def __unicode__(self):
+        return self.name
     def save(self):
         """Call for the creation of a repo first before saving model"""
         self.create_repo()
@@ -35,4 +56,12 @@ class Repo(models.Model):
         """Destroy the mercurial repo"""
         return bool(shutil.rmtree(settings.MERCURIAL_REPOS + self.name))
     class Admin:
-        pass
+        fields = (
+                  ('Repository Creation', {'fields': ('name', 'project',)}),
+                  ('Date information', {'fields': ('pub_date',)}),
+        )
+        list_display = ('name', 'project', 'pub_date',)
+        list_filter = ['pub_date', 'project',]
+        search_fields = ['longname', 'project']
+        date_hierarchy = 'pub_date'
+        ordering = ('pub_date',)
