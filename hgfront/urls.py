fcf871a984ce94605869fd14cd6eaca1a7a21cb9
Tane Piper <digitalspaghetti@gmail.com>
2007-12-12 13:16:27 +0000

    Removed registration from path, now is dependancy.  Updated URL's and settings to reflect Sign-Off: Tane Piper

--- hgfront/urls.py
+++ hgfront/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
 	url(r'^project/',include('hgfront.project.urls')),
-	url(r'^registration/',include('hgfront.registration.urls')),
+	url(r'^registration/',include('registration.urls')),
 )
