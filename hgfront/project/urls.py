2cc04b5e7aa8ea70fd772708f8e76c4077f6ab27
Tane Piper <digitalspaghetti@gmail.com>
2007-12-14 13:13:50 +0000

    + Added view for project details Sign-Off: Tane Piper

--- hgfront/project/urls.py
+++ hgfront/project/urls.py
@@ -7,25 +7,14 @@ from hgfront.project.models import Project, Repo
 project_list_dict = {
     'queryset':Project.objects.all(),
 }
-project_detail_dict = {
-    'queryset': Project.objects.all(),
-    'slug_field': 'shortname',
-    'extra_context':  {
-        'repos': Repo.objects.all().distinct()
-    }
-}
 
 # Generic Patterns
-urlpatterns = patterns('django.views.generic.list_detail',
-    url(r'^/?$', 'object_list', project_list_dict, name="project-list"),
-    url(r'^(?P<slug>[-\w]+)/$', 'object_detail', project_detail_dict, name="project-detail"),                      
+urlpatterns = patterns('',
+    url(r'^/?$', 'django.views.generic.list_detail.object_list', project_list_dict, name="project-list"),
+    url(r'^(?P<slug>[-\w]+)/$', 'hgfront.project.views.get_project_details', name="project-detail"),                      
 )
 
-# View Patterns
-urlpatterns += patterns('hgfront.project.views',
-    url(r'^(?P<slug>[-\w]+)/edit/$', 'project_edit', name="project-edit"),
-    url(r'^(?P<slug>[-\w]+)/delete/$', 'project_delete', name="project-delete"),                      
-)
+
 # Issues
 urlpatterns += patterns('',
     url(r'^(?P<slug>[-\w]+)/issues/', include('hgfront.issue.urls')),
--- hgfront/project/views.py
+++ hgfront/project/views.py
@@ -3,12 +3,12 @@ from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
+from django.views.generic.list_detail import object_detail
 
-from hgfront.project.models import Project
-def project_edit(request, slug):
-    return HttpResponse('editing of project %s' % slug)
-    #TODO: implement this
+from hgfront.project.models import Project, Repo
 
-def project_delete(request, slug):
-    return HttpResponse('deletion of project %s' % slug)
-    #TODO: implement this
+def get_project_details(request, slug):
+    project = Project.objects.filter(shortname__exact=slug)
+    repos = Repo.objects.all().filter(project__id=project[0].id)
+    return render_to_response('project/project_detail.html', {'project': project[0], 'repos': repos})
+    #return object_detail(request, queryset=project,extra_context={'repos': repos})
