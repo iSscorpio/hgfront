b4b266a5008172452b277c3bd299ad06fd3d5eee
Tane Piper <digitalspaghetti@gmail.com>
2007-12-14 15:34:14 +0000

    + Merge files + Adding new project creation form Sign-Off: Tane Piper

--- hgfront/project/views.py
+++ hgfront/project/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 from django.views.generic.list_detail import object_list
+from django.newforms import ModelForm
 
 from hgfront.project.models import Project, Repo
 
@@ -14,3 +15,13 @@ def get_project_list(request):
 def get_project_details(request, slug):
     project = get_object_or_404(Project, shortname__exact=slug)
     return render_to_response('project/project_detail.html', {'project': project})
+
+
+class ProjectCreateForm(ModelForm):
+    class Meta:
+        model=Project
+
+#def create_project_form(request):
+#    ProjectFormClass = forms.models.form_for_model(Project)
+#    form = ProjectFormClass()
+#    return form 
