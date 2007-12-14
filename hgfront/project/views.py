3c6abb3d9d657c0057d6a4c9f1256d0fd7cd839a
Tane Piper <digitalspaghetti@gmail.com>
2007-12-14 13:39:41 +0000

    + Improved project detail view query Sign-Off: Tane Piper

--- hgfront/project/views.py
+++ hgfront/project/views.py
@@ -8,7 +8,6 @@ from django.views.generic.list_detail import object_detail
 from hgfront.project.models import Project, Repo
 
 def get_project_details(request, slug):
-    project = Project.objects.filter(shortname__exact=slug)
-    repos = Repo.objects.all().filter(project__id=project[0].id)
-    return render_to_response('project/project_detail.html', {'project': project[0], 'repos': repos})
-    #return object_detail(request, queryset=project,extra_context={'repos': repos})
+    project = get_object_or_404(Project, shortname__exact=slug)
+    repos = Repo.objects.all().filter(project__id=project.id)
+    return render_to_response('project/project_detail.html', {'project': project, 'repos': repos})
