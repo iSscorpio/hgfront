a3d9612ef2f68a304cd91c27ced37fd82a0b1efb
Miran "Bonus" Lipovaca <miran.lipovaca@gmail.com>
2007-12-14 13:40:39 +0100

    + Added static serving and a new base.html along with a stylesheet

--- hgfront/urls.py
+++ hgfront/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 urlpatterns = patterns('',
 	url(r'^projects/',include('hgfront.project.urls')),
 	url(r'^registration/',include('registration.urls')),
+)
+#static content
+urlpatterns += patterns('',
+    url(r'^static/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.HGFRONT_STATIC_PATH})
 )
