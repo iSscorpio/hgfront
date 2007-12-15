f935dd435aa95ea0286d81e8b65782d98cda78b0
Tane Piper <digitalspaghetti@gmail.com>
2007-12-15 11:28:39 +0000

    + Added check that default permissions don't exist Sign-Off: Tane Piper

--- hgfront/project/signals.py
+++ hgfront/project/signals.py
@@ -8,7 +8,7 @@ def create_default_permission_set(sender, instance, signal, *args, **kwargs):
     Executed on creation of a project, this defines the default permissions
     """
     from hgfront.project.models import Project, ProjectPermissionSet
-    existing = ProjectPermissionSet.objects.all().filter(project=instance)
+    existing = ProjectPermissionSet.objects.all().filter(project=instance).filter(user=None)
     if not existing:
         permission_set = ProjectPermissionSet(is_default=True, project=instance, user=None)
         permission_set.save()
