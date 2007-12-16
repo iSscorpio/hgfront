1e1d595c0369aac55eb2630dad762cb5ab5863f2
Tane Piper <digitalspaghetti@gmail.com>
2007-12-16 10:35:41 +0000

    # A few minor fixes leading to repo screens Sign-Off: Tane Piper

--- hgfront/repo/urls.py
+++ hgfront/repo/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     url('^$','hgfront.repo.views.repo_list', name='repo-list'),
+    url('^create/$','hgfront.repo.views.repo_create', name='repo-create'),
     url('^(?P<repo_name>\w+)/$', 'hgfront.repo.views.repo_detail', name='repo-detail'),
 )
--- hgfront/repo/views.py
+++ hgfront/repo/views.py
@@ -3,5 +3,8 @@ from django.http import HttpResponse
 def repo_list(request, slug):
    return HttpResponse("repo list") 
 
-def repo_detail(request, repo_name, slug):
+def repo_detail(request, slug, repo_name):
    return HttpResponse("repo detail") 
+
+def repo_create(request, slug):
+    return HttpResponse("repo create")
