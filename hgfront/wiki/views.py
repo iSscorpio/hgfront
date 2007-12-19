3de09705d50b6294d5aa5d6b75df32da38bf10ea
Tane Piper <digitalspaghetti@gmail.com>
2007-12-19 08:44:26 +0000

    + Move improvements to Wiki code ! Currently wiki-edit reverse does not work Sign-Off: Tane Piper

--- hgfront/wiki/views.py
+++ hgfront/wiki/views.py
@@ -1,29 +1,32 @@
 # General Libraries
 # Django Libraries
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 # Project Libraries
 from hgfront.project.models import Project
 from hgfront.wiki.models import WikiPage
 from hgfront.wiki.forms import WikiPageCreateForm
+from hgfront.project.decorators import check_project_permission
 
-def wiki_index(request, slug):
-    """Return simple list of wiki pages"""
-    try:
-        page = WikiPage.objects.get(slug="Main_Page")
-        project = Project.objects.get(name_short__exact=slug)
-        return render_to_response('wiki/wiki_home.html', {'page':page, 'project':project})
-    except WikiPage.DoesNotExist:
-        return HttpResponseRedirect("/hgfront/projects/" + slug + "/wiki/edit/%s/" % "Main_Page/")
-
+@check_project_permission('view_wiki')
 def wiki_page(request, slug, page_name):
     """Return a Wiki page"""
     try:
         page = WikiPage.objects.get(slug=page_name)
-        return render_to_response('wiki/wiki_page.html', {'page':page})
+        project = Project.objects.get(name_short__exact=slug)
+        return render_to_response('wiki/wiki_page.html',
+                {
+                     'page':page,
+                     'project':project,
+                     'permissions': project.get_permissions(request.user),
+                }
+        )
     except WikiPage.DoesNotExist:
-        return HttpResponseRedirect("/hgfront/projects/" + slug + "/wiki/edit/%s/" % page_name)
-    
+        # FIXME: This does not redirect properly
+        return HttpResponseRedirect(reverse('wiki-edit', kwargs={'slug':slug, 'page_name':page_name}))
+
+@check_project_permission('edit_wiki')    
 def wiki_edit(request, slug, page_name):
     """Create or edit and Wiki page"""
     if request.method == "POST":
@@ -33,6 +36,12 @@ def wiki_edit(request, slug, page_name):
             return HttpResponseRedirect("/hgfront/projects/" + slug + "/wiki/%s/" % page_name)
     else:
         form = WikiPageCreateForm()
-    is_auth = bool(request.user.is_authenticated())
-    return render_to_response('wiki/wiki_create.html', {'form':form, 'is_auth': is_auth, 'project':slug, 'page_name':page_name})
+    return render_to_response('wiki/wiki_create.html',
+                {
+                     'form':form,
+                     'project':slug,
+                     'page_name':page_name,
+                     'permissions': project.get_permissions(request.user),
+                 }
+    )
  
