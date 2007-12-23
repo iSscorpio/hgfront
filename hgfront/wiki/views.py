37814d2316ac5c5a78be70fe3653f516834f9180
Miran "Bonus" Lipovaca <miran.lipovaca@gmail.com>
2007-12-23 17:53:40 +0100

    + Added wiki index page and fixed some wiki breadcrumbs

--- hgfront/wiki/views.py
+++ hgfront/wiki/views.py
@@ -2,7 +2,7 @@
 # Django Libraries
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 # Project Libraries
 from hgfront.project.models import Project
 from hgfront.wiki.models import WikiPage
@@ -11,7 +11,14 @@ from hgfront.project.decorators import check_project_permissions
 
 @check_project_permissions('view_wiki')
 def wiki_index(request, slug):
-    return HttpResponse('wiki index')
+    project = get_object_or_404(Project, name_short=slug)
+    permissions = project.get_permissions(request.user)
+    return render_to_response('wiki/wiki_home.html',
+        {
+            'project':project,
+            'permissions':permissions,
+        }
+    )
 
 @check_project_permissions('view_wiki')
 def wiki_page(request, slug, page_name):
@@ -27,13 +34,12 @@ def wiki_page(request, slug, page_name):
                 }
         )
     except WikiPage.DoesNotExist:
-        # FIXME: This does not redirect properly
         return HttpResponseRedirect(reverse('wiki-edit', kwargs={'slug':slug, 'page_name':page_name}))
 
 @check_project_permissions('edit_wiki')
 def wiki_edit(request, slug, page_name):
     """Create or edit and Wiki page"""
-    project = Project.objects.get(name_short=slug)
+    project = get_object_or_404(Project, name_short=slug)
     if request.method == "POST":
         form = WikiPageCreateForm(request.POST)
         if form.is_valid():
