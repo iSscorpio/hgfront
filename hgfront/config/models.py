f6baabcc77b80c6bc01464cf530814bcd15dc4c1
Tane Piper <digitalspaghetti@gmail.com>
2008-01-02 15:57:20 +0000

    + Added config application to store variables required through the site Sign-Off: Tane Piper

---
+++ hgfront/config/models.py
@@ -0,0 +1,20 @@
+# General Libraries
+
+# Django Libraries
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models import permalink, signals
+from django.dispatch import dispatcher
+
+# Project Libraries
+
+class InstalledStyles(models.Model):
+    """This stores a list of installed hgweb styles"""
+    short_name=models.CharField(max_length=50, db_index=True)
+    long_name=models.CharField(max_length=100)
+    
+class InstalledExtentions(models.Model):
+    """This stores a list of installed hg extentions"""
+    short_name=models.CharField(max_length=50, db_index=True)
+    long_name=models.CharField(max_length=100)
