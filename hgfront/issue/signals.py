295c8f6a428cbcf01dd9936d9a566af9b1283a18
Tane Piper <digitalspaghetti@gmail.com>
2008-01-06 22:10:50 +0000

    + Fixed issue with not sending email, required to be in list Sign-Off: Tane Piper

--- hgfront/issue/signals.py
+++ hgfront/issue/signals.py
@@ -8,5 +8,5 @@ from django.core.mail import EmailMessage
 def send_email_to_owner(sender, instance, signal, *args, **kwargs):
     """When a issue is added or changed, email the owner"""
     owner = User.objects.get(username=instance.user_posted)
-    email = EmailMessage(instance.title, instance.body, settings.ISSUE_FROM_EMAIL, owner.email)
-    email.send(False)
+    email = EmailMessage(instance.title, instance.body, settings.ISSUE_FROM_EMAIL, [owner.email])
+    email.send()
