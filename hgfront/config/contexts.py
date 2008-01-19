1bb694b8115a15ecee48e7b0bc848cebd61d49f7
Tane Piper <digitalspaghetti@gmail.com>
2008-01-19 15:27:49 +0000

    + Added context processor for issues_per_page Sign-Off: Tane Piper

--- hgfront/config/contexts.py
+++ hgfront/config/contexts.py
@@ -29,3 +29,8 @@ def hgf_repo_location(request):
     from hgfront.config.models import SiteOptions
     option = SiteOptions.objects.get(option_key = 'hgf_repo_location')
     return {'hgf_repo_location': option.option_value}
+    
+def hgf_issues_per_page(request):
+    from hgfront.config.models import SiteOptions
+    option = SiteOptions.objects.get(option_key = 'hgf_issues_per_page')
+    return {'hgf_issues_per_page': option.option_value}
