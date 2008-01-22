b291ab292c51970b5987762c44e389a3afe438ec
Tane Piper <digitalspaghetti@gmail.com>
2008-01-22 23:30:06 +0000

    + Improved new contexts for sidebar + Added random project context and element Sign-Off: Tane Piper

--- hgfront/project/contexts.py
+++ hgfront/project/contexts.py
@@ -1,8 +1,18 @@
+import datetime
+from random import randint
+
 from django.template import Context
 from hgfront.project.models import Project
 
-def num_projects(request):
+def project_stats(request):
     """
     This function returns the number of projects
     """
-    return {'hgf_num_projects': Project.objects.all().count()}
+    num_projects = Project.objects.all().count()
+    past24 = datetime.datetime.now() - datetime.timedelta(days=1)
+    
+    return dict([
+            ('hgf_num_projects', num_projects),
+            ('hgf_num_projects_last_24_hours', Project.objects.filter(pub_date__gt=past24).count()),
+            ('hgf_random_project', Project.objects.all()[randint(0, num_projects-1)]),
+        ])
