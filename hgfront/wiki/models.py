2b7b34d3fb402706053a486cad64284deb99a7de
Tane Piper <digitalspaghetti@gmail.com>
2008-01-29 18:38:09 +0000

    + Added change support for Wiki pages when save is made Sign-Off: Tane Piper
    --HG--
    branch : new_config

--- hgfront/wiki/models.py
+++ hgfront/wiki/models.py
@@ -5,6 +5,20 @@ class Page(models.Model):
     parent_project = models.ForeignKey(Project)
     name = models.CharField(max_length=255, primary_key=True)
     content = models.TextField(blank=True)
-    
+
+    def __unicode__(self):
+        page_name = ' '.join( self.name.split( '_' ) ).title()
+        return self.parent_project.name_long + '/' + page_name
+
+    class Admin:
+        pass
+
     class Meta:
         unique_together=('parent_project','name')
+
+class PageChange(models.Model):
+    page_id = models.ForeignKey(Page)
+    change_message = models.CharField(max_length=255)
+
+    class Admin:
+        pass
--- hgfront/wiki/views.py
+++ hgfront/wiki/views.py
@@ -1,4 +1,4 @@
-from hgfront.wiki.models import Page
+from hgfront.wiki.models import Page, PageChange
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 
@@ -9,8 +9,9 @@ def view_page(request, slug, page_name):
         page = Page.objects.get(pk=page_name)
     except Page.DoesNotExist:
         return render_to_response("wiki/create.html", {"project":slug, "page_name": page_name})
-    page_name = ' '.join( page_name.split( '_' ) ).title()
-    return render_to_response("wiki/page.html", {"project":slug,"page_name":page_name, "content":markdown.markdown(page.content)})
+    formatted_page_name = ' '.join( page_name.split( '_' ) ).title()
+    changesets = PageChange.objects.all().filter(page_id=page_name)
+    return render_to_response("wiki/page.html", {"project":slug, "page_name":page_name, "changesets":changesets, "formatted_page_name":formatted_page_name, "content":markdown.markdown(page.content)})
     
 def edit_page(request, slug, page_name):
     try:
@@ -30,5 +31,8 @@ def save_page(request, slug, page_name):
     except Page.DoesNotExist:
         page = Page(name=page_name, content=content)
     page.save()
+    if request.POST['change']:
+        changeset = PageChange(page_id=page, change_message = request.POST['change'])
+        changeset.save()
     
     return HttpResponseRedirect("/hgfront/projects/" + slug + "/wiki/" + page_name + "/")
