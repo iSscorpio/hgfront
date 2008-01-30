275cc13b89601a70c2b2ddc1ec959ef7ff0ce0c8
Miran Lipovaca <miran.lipovaca@gmail.com>
2008-01-30 18:21:14 +0100

    + Added permission checking to wiki views

--- hgfront/wiki/views.py
+++ hgfront/wiki/views.py
@@ -1,28 +1,32 @@
 from hgfront.wiki.models import Page, PageChange
 from hgfront.project.models import Project
-from django.shortcuts import render_to_response
+from hgfront.project.decorators import check_project_permissions
+from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
 
 import hgfront.markdown as markdown
 
+@check_project_permissions('view_wiki')
 def view_page(request, slug, page_name):
     try:
         page = Page.objects.get(pk=page_name)
     except Page.DoesNotExist:
         return render_to_response("wiki/create.html", {"project":slug, "page_name": page_name}, context_instance=RequestContext(request))
     page = Page.objects.get(name=page_name)
-    project = Project.objects.get(name_short__exact=slug)
+    project = get_object_or_404(Project, name_short=slug)
     return render_to_response("wiki/page.html", {"project":project, "page": page, "content":markdown.markdown(page.content)}, context_instance=RequestContext(request))
     
+@check_project_permissions('edit_wiki')
 def edit_page(request, slug, page_name):
     try:
         page = Page.objects.get(pk=page_name)
         content = page.content
     except Page.DoesNotExist:
         content = ""
-    return render_to_response("wiki/edit.html", {"project":slug, "page_name": page_name, "content":content}, context_instance=RequestContext(request))
+    return render_to_response("wiki/edit.html", {"project":get_object_or_404(Project, name_short=slug), "page_name": page_name, "content":content}, context_instance=RequestContext(request))
     
+@check_project_permissions('edit_wiki')
 def save_page(request, slug, page_name):
 
     content = request.POST['content']
@@ -39,7 +43,8 @@ def save_page(request, slug, page_name):
     
     return HttpResponseRedirect("/hgfront/projects/" + slug + "/wiki/" + page_name + "/")
     
+@check_project_permissions('view_wiki')
 def view_changes(request, slug, page_name):
-    project = Project.objects.get(name_short__exact=slug)
+    project = get_object_or_404(Project, name_short=slug)
     page = Page.objects.get(name=page_name)
     return render_to_response("wiki/changes.html", {"project":project, "page":page, "changesets": page.changesets()}, context_instance=RequestContext(request))
