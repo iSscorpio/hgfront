0ba3c81665d907231ab8031ba264726359bae3e6
mmarshall@swallow <none@none>
2008-03-20 20:25:23 -0400

    Added htpasswdutils.py that was used with freehg.org.  I'm not sure that I put it in the right place...

--- urls.py
+++ urls.py
@@ -5,3 +5,7 @@ urlpatterns = patterns('',
     url(r'^hgfront/',include('hgfront.urls')),
     url(r'^login/', 'django.contrib.auth.views.login', {'template_name':'login.html'}, name='login-screen')
 )
+
+from hgfront import htpasswdutils
+htpasswdutils.monkeypatch_user_model()
+
