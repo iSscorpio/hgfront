5163bff52b90fb64ff77e0ae28db26b3964a6c8c
Tane Piper <digitalspaghetti@gmail.com>
2008-03-24 18:03:54 +0000

    + More improvments to layout and functionality Tane Piper <digitalspaghetti@gmail.com>

--- hgfront/issue/urls.py
+++ hgfront/issue/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     url('^$','hgfront.issue.views.issue_list', name='issue-list'),
-    url('^create/$','hgfront.issue.views.issue_create', name='issue-create'),
     url('^(?P<issue_id>\d+)/$', 'hgfront.issue.views.issue_detail', name='issue-detail'),
     url('^(?P<issue_id>\d+)/edit/$', 'hgfront.issue.views.issue_edit', name='issue-edit'),
+    url('^create/$','hgfront.issue.views.issue_create', name='issue-create'),
 )
--- hgfront/project/decorators.py
+++ hgfront/project/decorators.py
@@ -26,6 +26,8 @@ def check_project_permissions(*args):
         from django.template.loader import Context, render_to_string
         def inner(*args, **kwargs):
             request = args[0]
+            # FIXME: Weird hack, had to add the print statement to stop the ORM failing on line 31
+            print request
             project_permissions = get_object_or_404(Project, name_short=kwargs['slug']).get_permissions(request.user)
             for permission in required_permissions:
                 if not getattr(project_permissions, permission):
