25e3e95af24f6de8651e940683b8e3276fbfedfd
Tane Piper <digitalspaghetti@gmail.com>
2008-04-06 16:25:45 +0100

    + Added table sorting to project list Tane Piper <digitalspaghetti@gmail.com>

--- hgfront/templates/project/js/project_list.js
+++ hgfront/templates/project/js/project_list.js
@@ -1,3 +1,5 @@
 $(document).ready(function(){
-	$('#project-list-table').tablesorter();	
+	$('#project-list-table')
+	.tablesorter({widthFixed: true, widgets: ['zebra']})
+	.tablesorterPager({container: $("#pager")});
 });
