fa2e892cb5d99127bbe79698aeb592a289570406
Tane Piper <digitalspaghetti@gmail.com>
2008-04-13 12:06:30 +0100

    - Removed install.sh file for queues, added management file to create on post sync Tane Piper <digitalspaghetti@gmail.com>

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#./manage.py syncdb
-curl -i http://127.0.0.1:8001/r/q/createqueue/ -d name=repoclone
-curl -i http://127.0.0.1:8001/r/q/createqueue/ -d name=repoupdate
---
+++ repo/management.py
@@ -0,0 +1,14 @@
+# Django Libraries
+from django.conf import settings
+from django.db.models import get_models, signals
+from django.dispatch import dispatcher
+
+def create_queues(app, created_models, verbosity, **kwargs):
+    """This creates the initial queues that mercural manager works with"""
+    from repo.models import Queue
+    if Queue in created_models:
+        Queue.objects.get_or_create(name='repoclone')
+        Queue.objects.get_or_create(name='repoupdate')
+        
+# Dispatchers       
+dispatcher.connect(create_queues, signal=signals.post_syncdb)
