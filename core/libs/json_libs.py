152010e7823275ac5e34ed2d0c878779655c0953
Tane Piper <digitalspaghetti@gmail.com>
2008-04-15 22:14:54 +0100

    - Removed and moved files Tane Piper <digitalspaghetti@gmail.com>

--- core/libs/json_libs.py
+++ core/libs/json_libs.py
@@ -1,9 +1,17 @@
+import datetime
+from decimal import Decimal
+
+from django.conf import settings
+from django.core.serializers import serialize
 from django.core.serializers.json import DateTimeAwareJSONEncoder
 from django.db import models
+from django.db.models import signals
+from django.db.models.query import QuerySet
+from django.dispatch import dispatcher
+from django.http import HttpResponse
+from django.utils import simplejson
 from django.utils.functional import Promise
 from django.utils.encoding import force_unicode
-from django.utils import simplejson as json
-from decimal import Decimal
 
 
 def json_encode(data):
@@ -67,4 +75,55 @@ def json_encode(data):
     
     ret = _any(data)
     
-    return json.dumps(ret, cls=DateTimeAwareJSONEncoder)
+    return simplejson.dumps(ret, cls=DateTimeAwareJSONEncoder)
+
+class JsonResponse(HttpResponse):
+    def __init__(self, object):
+        if isinstance(object, QuerySet):
+            content = serialize('json', object)
+        else:
+            content = simplejson.dumps(object)
+        super(JsonResponse, self).__init__(content, mimetype='application/json')
+        
+class JSONEncoder(simplejson.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            return obj.strftime('%Y-%m-%d %H:%M:%S')
+        elif isinstance(obj, datetime.date):
+            return obj.strftime('%Y-%m-%d')
+        elif isinstance(obj, datetime.time):
+            return obj.strftime('%H:%M:%S')
+        return simplejson.JSONEncoder.default(self, obj)
+        
+def dumps(data):
+    return JSONEncoder().encode(data)
+    
+def loads(str):
+    return simplejson.loads(str, encoding=settings.DEFAULT_CHARSET)
+    
+class JSONField(models.TextField):
+    def db_type(self):
+        return 'text'
+        
+    def pre_save(self, model_instance, add):
+        value = getattr(model_instance, self.attname, None)
+        return dumps(value)
+    
+    def contribute_to_class(self, cls, name):
+        super(JSONField, self).contribute_to_class(cls, name)
+        dispatcher.connect(self.post_init, signal=signals.post_init, sender=cls)
+        
+        def get_json(model_instance):
+            return dumps(getattr(model_instance, self.attname, None))
+        setattr(cls, 'get_%s_json' % self.name, get_json)
+    
+        def set_json(model_instance, json):
+            return setattr(model_instance, self.attname, loads(json))
+        setattr(cls, 'set_%s_json' % self.name, set_json)
+    
+    def post_init(self, instance=None):
+        value = self.value_from_object(instance)
+        if (value):
+            setattr(instance, self.attname, loads(value))
+        else:
+            setattr(instance, self.attname, None)
